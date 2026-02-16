35c6f116e328a034c36363ccb22020e063e8efc4
SHAKIB AHMMED <shakibahmmed1@gmail.com>
2026-02-16 19:42:35 +0600

    all components are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import RoomDetails from "./pages/RoomDetails";
+import Booking from "./pages/Booking";
+
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import CheckIn from "./pages/admin/CheckIn";
+import CheckOut from "./pages/admin/CheckOut";
+import AllBookings from "./pages/admin/AllBookings";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/rooms/:id" element={<RoomDetails />} />
+        <Route path="/booking/:roomId" element={<Booking />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/checkin" element={<CheckIn />} />
+        <Route path="/admin/checkout" element={<CheckOut />} />
+        <Route path="/admin/bookings" element={<AllBookings />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
